30a59d702765a836e8a3d616921663fcb4d641d1
Jennifer <jennifer.gd42@gmail.com>
2016-07-24 21:41:58 +0000

    Able to get jobs without duplicate postings.

--- jobs.py
+++ jobs.py
@@ -25,25 +25,35 @@ def read_jobs_from_file(filepath):
     Returned dictionary is formatted as {unique_id: Job object}."""
 
     jobs = {}
+    positions_to_post = []
 
     # Open the passed file and iterate over it line by line
     for line in open(filepath):
 
         # Unpack the fields from the file into three variables
         (unique_id, title, description) = line.strip().split("|")
+        print "****"
+        print title
+        print "****"
 
-        if title in jobs.keys():
-            print "|{} already in the list of jobs to be posted.|".format(
+        if title in positions_to_post:
+            print "OMG IM IN THE IF"
+            print "{} already in the list of jobs to be posted.".format(
                   title)
             continue
+
         else:
+            # Track the positions we've added so far to help avoid
+            # accidental repeat listings.
+            positions_to_post.append(title)
+
             # Cast the job ID to an int to work with it more easily
             unique_id = int(unique_id)
 
             # Store the job in the dictionary
             jobs[unique_id] = Job(unique_id, title, description)
 
-        return jobs
+    return jobs
 
 jobs_to_post = read_jobs_from_file("jobs.txt")
-print jobs_to_post
+print "POST THESE: ", jobs_to_post
