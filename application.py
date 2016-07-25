89b404b992b48278783532cab78b651e37188a6b
Jennifer <jennifer.gd42@gmail.com>
2016-07-25 00:15:25 +0000

    Added functionality to redirect users on form submit, so they won't risk resubmitting data if they refresh the page.

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, session, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 
 import jinja2
@@ -39,15 +39,31 @@ def submit_application():
     # Prettify the salary with commas.
     prettified_salary_req = "{:,}".format(salary_requirement)
 
-    # Queue a flash message to show the user.
-    flash("Your application has been submitted! Don't refresh the page.")
+    # Store applicant data in a session so we can redirect them on submit
+    # and still retain the data.
+    session["first_name"] = first_name
+    session["last_name"] = last_name
+    session["desired_position"] = desired_position
+    session["salary_requirement"] = prettified_salary_req
 
-    # Pass Jinja all the user information to fill out the response template.
+    # Queue a flash message to show the user.
+    flash("Your application has been submitted!")
+
+    return redirect("/application-response")
+
+
+@app.route("/application-response")
+def respond_to_application_submission():
+    """Displays the applicant's data back to them in a page where
+    refreshing won't cause them to resubmit form data.
+    """
+
+    # Pass all the session data to Jinja.
     return render_template("application-response.html",
-                           first=first_name,
-                           last=last_name,
-                           position=desired_position,
-                           salary_req=prettified_salary_req)
+                           first=session["first_name"],
+                           last=session["last_name"],
+                           position=session["desired_position"],
+                           salary_req=session["salary_requirement"])
 
 
 if __name__ == "__main__":
